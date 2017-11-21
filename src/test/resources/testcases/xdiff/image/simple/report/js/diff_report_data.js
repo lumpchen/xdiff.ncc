566a9ef97e46cc586ce0a8298633cb1f1dbae9f1
Mike Li <lumpchen@gmail.com>
2017-11-21 16:03:24 +0800

    update testcase

--- src/test/resources/testcases/xdiff/image/simple/report/js/diff_report_data.js
+++ src/test/resources/testcases/xdiff/image/simple/report/js/diff_report_data.js
@@ -1,309 +1,209 @@
 var PDF_DIFF = PDF_DIFF || {};
 PDF_DIFF.diff_report_data = {
+    "test_pdf_json_obj": {
+        "pageCount": 1,
+        "fileName": "test.pdf",
+        "keywords": "",
+        "author": "",
+        "subject": "",
+        "created": "Tue Nov 21 15:24:23 CST 2017",
+        "title": "",
+        "version": "1.5",
+        "pages": [{
+            "num": 0,
+            "width": 595.3200073242188,
+            "imageTag": "test-0.png",
+            "height": 841.9199829101562
+        }],
+        "fileSize": "631177 Bytes",
+        "modified": "Tue Nov 21 15:24:23 CST 2017",
+        "producer": "Microsoft® Word 2013",
+        "location": "C:\\dev\\mine\\git\\xdiff.ncc\\src\\test\\resources\\testcases\\xdiff\\image\\simple\\test.pdf"
+    },
+    "Base_Stroke_Color": "red",
+    "Base_Fill_Color": "rgba(138, 43, 226, 0.2)",
+    "Test_Fill_Color": "rgba(138, 43, 226, 0.2)",
+    "FileFormat": "pdf",
+    "diff_accepted_page_nums": [],
+    "ShowDifferentPagesOnly": false,
     "Test_Stroke_Color": "red",
-    "diff_page_nums": [0],
+    "diff_page_count": 1,
     "page_xor_images": {"0": "0-xor.png"},
+    "Rendering_Resolution": 96,
+    "diff_page_nums": [0],
+    "base_pdf_json_obj": {
+        "pageCount": 1,
+        "fileName": "control.pdf",
+        "keywords": "",
+        "author": "",
+        "subject": "",
+        "created": "Tue Nov 21 15:05:03 CST 2017",
+        "title": "",
+        "version": "1.5",
+        "pages": [{
+            "num": 0,
+            "width": 595.3200073242188,
+            "imageTag": "base-0.png",
+            "height": 841.9199829101562
+        }],
+        "fileSize": "631909 Bytes",
+        "modified": "Tue Nov 21 15:05:03 CST 2017",
+        "producer": "Microsoft® Word 2013",
+        "location": "C:\\dev\\mine\\git\\xdiff.ncc\\src\\test\\resources\\testcases\\xdiff\\image\\simple\\control.pdf"
+    },
     "diff_content_json_obj": [{
+        "PageNo": 0,
         "Result": {
-            "Accept_Difference": [],
-            "Text": [{
-                "Attributes": [{
-                    "Value": [
-                        "#INSERT#",
-                        "add test text"
-                    ],
-                    "Key": "Text",
-                    "Equals": false
-                }],
-                "SubOutline": [
-                    [],
-                    []
-                ],
-                "Outline": [
-                    [],
-                    [
-                        110.52,
-                        567.178,
-                        55.652,
-                        7.563
-                    ]
-                ],
-                "AcceptDiffRegion": [
-                    [],
-                    []
-                ]
-            }],
+            "Path": [],
+            "Annot": [],
             "Page": [],
+            "Text": [],
             "Image": [
                 {
-                    "Attributes": [
-                        {
-                            "Value": [
-                                "352",
-                                "1500"
-                            ],
-                            "Key": "Width",
-                            "Equals": false
-                        },
-                        {
-                            "Value": [
-                                "147",
-                                "1050"
-                            ],
-                            "Key": "Height",
-                            "Equals": false
-                        },
-                        {
-                            "Value": [
-                                "155232",
-                                "4725000"
-                            ],
-                            "Key": "Byte Count",
-                            "Equals": false
-                        },
-                        {
-                            "Value": [
-                                "8",
-                                "8"
-                            ],
-                            "Key": "BitsPerComponent",
-                            "Equals": true
-                        },
-                        {
-                            "Value": [
-                                "jpg",
-                                "png"
-                            ],
-                            "Key": "Suffix",
-                            "Equals": false
-                        },
-                        {
-                            "Value": [
-                                "x=72.0, y=587.57, width=272.25, height=118.5",
-                                "x=72.0, y=580.07, width=180.0, height=126.0"
-                            ],
-                            "Key": "Position & Size",
-                            "Equals": false
-                        }
-                    ],
                     "SubOutline": [
                         [],
                         []
                     ],
-                    "Outline": [
-                        [
-                            72,
-                            587.57,
-                            272.25,
-                            118.5
-                        ],
-                        [
-                            72,
-                            580.07,
-                            180,
-                            126
-                        ]
-                    ],
                     "AcceptDiffRegion": [
                         [],
                         []
-                    ]
-                },
-                {
+                    ],
                     "Attributes": [
                         {
+                            "Equals": false,
                             "Value": [
-                                "850",
-                                "850"
+                                "191",
+                                "276"
                             ],
-                            "Key": "Width",
-                            "Equals": true
+                            "Key": "Width"
                         },
                         {
+                            "Equals": false,
                             "Value": [
-                                "256",
-                                "256"
+                                "56",
+                                "72"
                             ],
-                            "Key": "Height",
-                            "Equals": true
+                            "Key": "Height"
                         },
                         {
+                            "Equals": false,
                             "Value": [
-                                "652800",
-                                "652800"
+                                "32088",
+                                "59616"
                             ],
-                            "Key": "Byte Count",
-                            "Equals": true
+                            "Key": "Byte Count"
                         },
                         {
+                            "Equals": true,
                             "Value": [
                                 "8",
                                 "8"
                             ],
-                            "Key": "BitsPerComponent",
-                            "Equals": true
+                            "Key": "BitsPerComponent"
                         },
                         {
+                            "Equals": true,
                             "Value": [
-                                "jpg",
+                                "png",
+                                "png"
+                            ],
+                            "Key": "Suffix"
+                        },
+                        {
+                            "Equals": true,
+                            "Value": [
+                                "x=227.28, y=277.32, width=98.28, height=20.4",
+                                "x=227.28, y=277.32, width=98.28, height=20.4"
+                            ],
+                            "Key": "Position & Size"
+                        }
+                    ],
+                    "Outline": [
+                        [
+                            227.28,
+                            277.32,
+                            98.28,
+                            20.4
+                        ],
+                        [
+                            227.28,
+                            277.32,
+                            98.28,
+                            20.4
+                        ]
+                    ]
+                },
+                {
+                    "SubOutline": [
+                        [],
+                        []
+                    ],
+                    "AcceptDiffRegion": [
+                        [],
+                        []
+                    ],
+                    "Attributes": [
+                        {
+                            "Equals": false,
+                            "Value": [
+                                "",
+                                "387"
+                            ],
+                            "Key": "Width"
+                        },
+                        {
+                            "Equals": false,
+                            "Value": [
+                                "",
+                                "290"
+                            ],
+                            "Key": "Height"
+                        },
+                        {
+                            "Equals": false,
+                            "Value": [
+                                "",
+                                "336690"
+                            ],
+                            "Key": "Byte Count"
+                        },
+                        {
+                            "Equals": false,
+                            "Value": [
+                                "",
+                                "8"
+                            ],
+                            "Key": "BitsPerComponent"
+                        },
+                        {
+                            "Equals": false,
+                            "Value": [
+                                "",
                                 "jpg"
                             ],
-                            "Key": "Suffix",
-                            "Equals": true
+                            "Key": "Suffix"
                         },
                         {
+                            "Equals": false,
                             "Value": [
-                                "x=72.0, y=437.44, width=362.4, height=126.88",
-                                "x=72.0, y=442.38, width=362.1, height=121.94"
+                                "",
+                                "x=432.6, y=659.88, width=114.6, height=82.32"
                             ],
-                            "Key": "Position & Size",
-                            "Equals": false
+                            "Key": "Position & Size"
                         }
-                    ],
-                    "SubOutline": [
-                        [],
-                        []
-                    ],
-                    "Outline": [
-                        [
-                            72,
-                            437.44,
-                            362.4,
-                            126.88
-                        ],
-                        [
-                            72,
-                            442.38,
-                            362.1,
-                            121.94
-                        ]
-                    ],
-                    "AcceptDiffRegion": [
-                        [],
-                        []
-                    ]
-                },
-                {
-                    "Attributes": [
-                        {
-                            "Value": [
-                                "",
-                                "369"
-                            ],
-                            "Key": "Width",
-                            "Equals": false
-                        },
-                        {
-                            "Value": [
-                                "",
-                                "90"
-                            ],
-                            "Key": "Height",
-                            "Equals": false
-                        },
-                        {
-                            "Value": [
-                                "",
-                                "99630"
-                            ],
-                            "Key": "Byte Count",
-                            "Equals": false
-                        },
-                        {
-                            "Value": [
-                                "",
-                                "8"
-                            ],
-                            "Key": "BitsPerComponent",
-                            "Equals": false
-                        },
-                        {
-                            "Value": [
-                                "",
-                                "png"
-                            ],
-                            "Key": "Suffix",
-                            "Equals": false
-                        },
-                        {
-                            "Value": [
-                                "",
-                                "x=72.0, y=268.01, width=276.75, height=67.5"
-                            ],
-                            "Key": "Position & Size",
-                            "Equals": false
-                        }
-                    ],
-                    "SubOutline": [
-                        [],
-                        []
                     ],
                     "Outline": [
                         [],
                         [
-                            72,
-                            268.01,
-                            276.75,
-                            67.5
+                            432.6,
+                            659.88,
+                            114.6,
+                            82.32
                         ]
-                    ],
-                    "AcceptDiffRegion": [
-                        [],
-                        []
                     ]
                 }
             ],
-            "Annot": [],
-            "Path": []
-        },
-        "PageNo": 0
-    }],
-    "diff_page_count": 1,
-    "diff_accepted_page_nums": [],
-    "FileFormat": "pdf",
-    "ShowDifferentPagesOnly": true,
-    "Test_Fill_Color": "rgba(138, 43, 226, 0.2)",
-    "test_pdf_json_obj": {
-        "fileSize": "286362 Bytes",
-        "author": "",
-        "pageCount": 1,
-        "title": "",
-        "keywords": "",
-        "location": "C:\\dev\\mine\\git\\xdiff\\src\\test\\resources\\testcases\\xdiff\\image\\simple\\test.pdf",
-        "created": "Thu Mar 17 20:00:21 CST 2016",
-        "subject": "",
-        "pages": [{
-            "num": 0,
-            "imageTag": "test-0.png",
-            "height": 792,
-            "width": 612
-        }],
-        "producer": "xPression v4.5 / dcpi_BUILD_4.6.1.3.19 / JPDF by CambridgeDocs",
-        "fileName": "test.pdf",
-        "version": "1.5",
-        "modified": "Thu Mar 17 20:00:21 CST 2016"
-    },
-    "Base_Stroke_Color": "red",
-    "base_pdf_json_obj": {
-        "fileSize": "252142 Bytes",
-        "author": "",
-        "pageCount": 1,
-        "title": "",
-        "keywords": "",
-        "location": "C:\\dev\\mine\\git\\xdiff\\src\\test\\resources\\testcases\\xdiff\\image\\simple\\control.pdf",
-        "created": "Tue Mar 15 20:00:32 CST 2016",
-        "subject": "",
-        "pages": [{
-            "num": 0,
-            "imageTag": "base-0.png",
-            "height": 792,
-            "width": 612
-        }],
-        "producer": "xPression v4.5 / dcpi_BUILD_4.6.1.3.19 / JPDF by CambridgeDocs",
-        "fileName": "control.pdf",
-        "version": "1.5",
-        "modified": "Tue Mar 15 20:00:32 CST 2016"
-    },
-    "Base_Fill_Color": "rgba(138, 43, 226, 0.2)",
-    "Rendering_Resolution": 96
+            "Accept_Difference": []
+        }
+    }]
 }
